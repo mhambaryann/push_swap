09255f8982965882f1a47276d2521f288ec3557c
Margarita Hambaryan <mhambary@c2r14s1.42yerevan.am>
2024-06-11 17:11:00 +0400

    added ft_lstnew.c file

--- error_management.c
+++ error_management.c
@@ -6,10 +6,29 @@
 /*   By: mhambary <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 15:27:25 by mhambary          #+#    #+#             */
-/*   Updated: 2024/06/09 16:14:43 by mhambary         ###   ########.fr       */
+/*   Updated: 2024/06/11 17:10:21 by mhambary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	is_number(char *current_number)
+{
+	int	i;
 
+	i = 0;
+	if (str[0] == '-')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	validate_arguments(int argc, char **argv)
+{
+
+}
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,35 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhambary <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/11 16:15:09 by mhambary          #+#    #+#             */
-/*   Updated: 2024/06/11 17:10:18 by mhambary         ###   ########.fr       */
+/*   Created: 2024/06/11 16:08:59 by mhambary          #+#    #+#             */
+/*   Updated: 2024/06/11 16:29:57 by mhambary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-# include <limits.h>
-
-char	**ft_split(char *str);
-int		ft_isdigit(int c);
-void	free_out(char **out);
-char	*join_args(int argc, char **argv);
-int		ft_strlen(char *str);
-
-typedef struct s_stack
+t_stack	*ft_lstnew(int value)
 {
-	int				value;
-	int				index;
-	struct s_stack	*next;
-}	t_stack;
+	t_stack	*new_node;
 
-t_stack	*ft_lstnew(int value);
-#endif
+	new_node = (t_stack *)malloc(sizeof(t_stack));
+	if (!new_node)
+		return (NULL);
+	new_node->value = value;
+	new_node->next = NULL;
+	return (new_node);
+}
