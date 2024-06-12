2228a67327488efa6875eadce9c8ebd6e01e6ad3
Margarita Hambaryan <mhambaryann@gmail.com>
2024-06-12 15:20:56 +0400

    added number management

--- error_management.c
+++ error_management.c
@@ -17,18 +17,42 @@ int	is_number(char *current_number)
 	int	i;
 
 	i = 0;
-	if (str[0] == '-')
-		i++;
-	while (str[i])
+	if (current_number[0] == '-')
 	{
-		if (!ft_isdigit(str[i]))
+		if (i >= 2)
+			return (0);
+		i++;
+	}
+	if (current_number[i] == '\0')
+		return (0);
+	while (current_number[i])
+	{
+		if (!ft_isdigit(current_number[i]))
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-int	validate_arguments(int argc, char **argv)
+int	len_splitted_array(char **out)
 {
+	int	len;
+
+	len = 0;
+	while(out[len] != NULL)
+		len++;
+	return (len);
+}
+
+int	validate_arguments(char **out)
+{
+	long int	*numbers;
+	int			len;
+
+	len = len_splitted_array(out);
+	numbers = (long int*)malloc(sizeof(long int*) * len);
+	if (!numbers)
+		return (NULL);
+
 
 }
